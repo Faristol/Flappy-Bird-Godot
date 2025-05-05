362628c4fa53baedd70df7bfe04a5a97edb9c9f1
Faristol <joseptortosa33@gmail.com>
2025-05-05 22:21:06 +0200

    more

--- scripts/bird.gd
+++ scripts/bird.gd
@@ -2,7 +2,14 @@ extends CharacterBody2D
 
 const GRAVITY = 900.0
 const JUMP_FORCE = -300.0
-
+const START_POS = Vector2(100,400)
+func _ready():
+	reset()
+	
+func reset():
+	position = START_POS
+	set_rotation(0)
+	
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
 	if Input.is_action_just_pressed("space_jump") or Input.is_action_just_pressed("mouse1_jump"):
