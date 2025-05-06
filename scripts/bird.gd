a0b75c6fac289354dc73d0643d2280008e29f19e
Faristol <joseptortosa33@gmail.com>
2025-05-06 18:36:33 +0200

    moving ground, and some move animation

--- scripts/bird.gd
+++ scripts/bird.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
+@onready var _animated_sprite = $AnimatedSprite2D
 const GRAVITY = 900.0
-const JUMP_FORCE = -300.0
+const JUMP_FORCE = -400.0
 const START_POS = Vector2(100,400)
 func _ready():
 	reset()
@@ -14,4 +15,16 @@ func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
 	if Input.is_action_just_pressed("space_jump") or Input.is_action_just_pressed("mouse1_jump"):
 		velocity.y = JUMP_FORCE
+		
+		
+		
+	#quan "caiga" cesar la animacio
+	if velocity.y > 0:
+		_animated_sprite.stop()
+		set_rotation(0.5)
+	else:
+		_animated_sprite.play("flying")
+		set_rotation(-0.5)
+	
+
 	move_and_slide()
---
+++ scripts/ground.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var ground_vel: float = 300.0
+const BACKGROUND_WIDTH := 434.0
+func _process(delta):
+	#el piso avança cap a l'esquerra, xaq es cree l'efecte visual
+	#q el pardal avança cap a la dreta
+	#el background te una amplaria de 434
+	#el groun una amplaria de 868
+	#quan la posiciç´
+	position.x -= delta * ground_vel
+	if position.x <= -BACKGROUND_WIDTH:
+		position.x += BACKGROUND_WIDTH
+		
+	
+	
+	
